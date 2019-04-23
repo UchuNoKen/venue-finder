66138537b1bb5bd4d8d5c12e1fc09924dfab2509
UchuNoKen <xen.o.fite@gmail.com>
2019-04-23 12:57:34 -0400

    added AppBar

--- src/components/header_footer/header.js
+++ src/components/header_footer/header.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/core/Menu";
+import IconButton from "@material-ui/core/IconButton";
+
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return <AppBar positon="fixed">Header</AppBar>;
   }
 }
 
