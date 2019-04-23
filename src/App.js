f281456a9ddb6ec5b483252027c6c6e32f25018d
UchuNoKen <xen.o.fite@gmail.com>
2019-04-23 12:53:01 -0400

    import header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 
+import Header from "./components/header_footer/Header";
+
 class App extends Component {
   render() {
-    return <div className="App">Hilo there</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
