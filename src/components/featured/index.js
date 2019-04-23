486261c169ca26a8c361ea2f250ae9ce245c29c6
UchuNoKen <xen.o.fite@gmail.com>
2019-04-23 16:07:55 -0400

    add TimeUntil component, add timer, hard-code nums

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "./Carousel";
+import TimeUntil from "./TimeUntil";
 
 const Featured = () => {
   return (
@@ -9,6 +10,8 @@ const Featured = () => {
       <div className="artist_name">
         <div className="wrapper">Ariana Grande</div>
       </div>
+
+      <TimeUntil />
     </div>
   );
 };
