7cd40d702cbf9044d65c94ebce895af1baa3ad01
UchuNoKen <xen.o.fite@gmail.com>
2019-04-23 22:36:20 -0400

    add venue calendar item

--- src/components/venyouInfo/index.js
+++ src/components/venyouInfo/index.js
@@ -4,7 +4,35 @@ import icon_calendar from "../../resources/images/icons/calendar.png";
 import icon_location from "../../resources/images/icons/location.png";
 
 const VenyouInfo = () => {
-  return <div />;
+  return (
+    <div className="bck_black">
+      <div className="center_wrapper">
+        <div className="vn_wrapper">
+          <div className="vn_item">
+            <div className="vn_outer">
+              <div className="vn_inner">
+                <div className="vn_icon_square bck_red" />
+                <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
+                <div className="vn_title">Event Date & Time</div>
+                <div className="vn_desc"> Dec. 16, 2019 @ 9pm</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="vn_item">
+            <div className="vn_outer">
+              <div className="vn_inner">
+                <div className="vn_icon_square bck_red" />
+                <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
+                <div className="vn_title">Event Date & Time</div>
+                <div className="vn_desc"> Dec. 16, 2019 @ 9pm</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VenyouInfo;
