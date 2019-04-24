88142a6078fe0304e13fa142ad3043c5e8c983b9
UchuNoKen <xen.o.fite@gmail.com>
2019-04-23 22:22:26 -0400

    add venyouInfo dir, create index.js

---
+++ src/components/venyouInfo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import icon_calendar from "../../resources/images/icons/calendar.png";
+import icon_location from "../../resources/images/icons/location.png";
+
+const VenyouInfo = () => {
+  return <div />;
+};
+
+export default VenyouInfo;
