a706137e66b384ca4aaf5e5d246de2c82969e7fa
UchuNoKen <xen.o.fite@gmail.com>
2019-04-23 23:03:05 -0400

    add calendar and location icons

--- src/components/venyouInfo/index.js
+++ src/components/venyouInfo/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Zoom from "react-reveal/Zoom";
 
 import icon_calendar from "../../resources/images/icons/calendar.png";
 import icon_location from "../../resources/images/icons/location.png";
@@ -8,27 +9,31 @@ const VenyouInfo = () => {
     <div className="bck_black">
       <div className="center_wrapper">
         <div className="vn_wrapper">
-          <div className="vn_item">
-            <div className="vn_outer">
-              <div className="vn_inner">
-                <div className="vn_icon_square bck_red" />
-                <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
-                <div className="vn_title">Event Date & Time</div>
-                <div className="vn_desc"> Dec. 16, 2019 @ 9pm</div>
+          <Zoom duration={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_red" />
+                  <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
+                  <div className="vn_title">Event Date & Time</div>
+                  <div className="vn_desc"> Dec. 16, 2019 @ 9pm</div>
+                </div>
               </div>
             </div>
-          </div>
+          </Zoom>
 
-          <div className="vn_item">
-            <div className="vn_outer">
-              <div className="vn_inner">
-                <div className="vn_icon_square bck_red" />
-                <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
-                <div className="vn_title">Event Date & Time</div>
-                <div className="vn_desc"> Dec. 16, 2019 @ 9pm</div>
+          <Zoom duration={500} delay={300}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_yellow" />
+                  <div className="vn_icon" style={{ background: `url(${icon_location})` }} />
+                  <div className="vn_title">Event Location</div>
+                  <div className="vn_desc"> State Farm Arena, Atlanta, GA</div>
+                </div>
               </div>
             </div>
-          </div>
+          </Zoom>
         </div>
       </div>
     </div>
